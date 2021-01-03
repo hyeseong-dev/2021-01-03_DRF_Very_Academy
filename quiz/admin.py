41455bc2ebd6787245735c122f6346d8763efbf3
이혜성 <hyeseong43@gmail.com>
2021-01-03 21:48:43 +0900

    Step -1 Start Django Project - Setup Step -2 Models Step -3 Configure Admin Step -4 Views and Serializers

---
+++ quiz/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from quiz           import models
+
+@admin.register(models.Category)
+class CatAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+@admin.register(models.Quizzes)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title',]
+
+
+class AnswerInlineModel(admin.TabularInline):
+    model  = models.Answer
+    fields = ['answer_text','is_right']
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields       = ['title','quiz']
+    list_display = ['title', 'quiz', 'date_created']
+    inlines      = [AnswerInlineModel,]
+
+@admin.register(models.Answer)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['answer_text', 'is_right', 'question']
